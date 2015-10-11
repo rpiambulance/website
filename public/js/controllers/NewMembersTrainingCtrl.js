293b530abd52f8684617e9730b4c020fde6fca48
Justin Etzine <justetz@gmail.com>
2015-10-10 23:57:51 -0400

    Implemented view/edit scheme for new members training view

--- public/js/controllers/NewMembersTrainingCtrl.js
+++ public/js/controllers/NewMembersTrainingCtrl.js
@@ -1,4 +1,15 @@
 var ctrl_name = 'NewMembersTrainingCtrl';
 angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
-
+    $scope.pageHeader = 'New Member Training';
+    $scope.sections = [
+        {
+            header: '',
+            internal_title: 'Text',
+            body: 'Members that are new to RPI Ambulance start out as observers. Observers do not take part in patient ' +
+            'care, and are only there to observe and learn about the operations and the equipment on the ambulance. ' +
+            'he goal of all observers is to be promoted to Attendant. All New Members must go through a mandatory ' +
+            'OSHA (Occupational Safety and Health Administration) safety training, a HIPAA (Health Insurance ' +
+            'Portability and Accountability Act) patient confidentiality training and and RPI Ambulance Orientation Class.'
+        }
+    ];
 }]);
