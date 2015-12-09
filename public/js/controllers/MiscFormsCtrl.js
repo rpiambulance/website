3a9343453476b87190dc396496971673444be596
David-Sparky <sparkd@rpi.edu>
2015-12-09 15:50:09 -0500

    Misc forms ready for actual files. Report issues needs form now

--- public/js/controllers/MiscFormsCtrl.js
+++ public/js/controllers/MiscFormsCtrl.js
@@ -1,5 +1,21 @@
-var ctrl_name = 'MiscFormsCtrl';
-angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
+angular.module('MiscFormsCtrl', []).controller('MiscFormsCtrl', ['$scope', function($scope) {
     $scope.pageHeader = 'Miscellaneous Forms';
-    $scope.sections = [ { header: '', internal_title: 'Links', body: 'LINKS HERE', centered: true } ];
+    $scope.columns = false;
+
+    $scope.sections = [
+        {
+            header: 'Checklists',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe frameborder="0" class="embed-responsive-item" style="width: 100% !important; height: 100% !important;"' +
+            'src="https://drive.google.com/embeddedfolderview?id=0B3mvXB0aR4DiejdzdG1TQlI1QWc#list"></iframe></div>'
+        },
+
+        {
+            header: 'Training Forms',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe src="https://drive.google.com/embeddedfolderview?id=0B7_ZvoVxWf0qVUE0TDVuSXlOYnM" width="800" height="600" frameborder="0"></iframe>'
+
+        }
+
+    ];
 }]);
--- public/js/controllers/ReportIssueCtrl.js
+++ public/js/controllers/ReportIssueCtrl.js
@@ -1,4 +1,12 @@
-var ctrl_name = 'ReportIssueCtrl';
-angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
+angular.module('ReportIssueCtrl', []).controller('ReportIssueCtrl', ['$scope', function($scope) {
+    $scope.pageHeader = 'Report an Issue';
+    $scope.sections = [
+        {
+            header: 'We\'re so sorry something went wrong...',
+            internal_title: 'Message',
+            body: 'We\'re so sorry something isn\'t working quite right. Please use the following form to tell us more' +
+                ' about the problem you\'ve just encountered.'
+        }
 
+    ];
 }]);
