353dc95e9be3a141aa1ac647aa378bbcfdb1962a
David-Sparky <sparkd@rpi.edu>
2015-12-09 17:14:00 -0500

    Added constitution

--- public/js/controllers/ConstitutionCtrl.js
+++ public/js/controllers/ConstitutionCtrl.js
@@ -7,8 +7,8 @@ angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) 
             internal_title: 'About Text',
             body: 'The Constitution of RPI Ambulance is our governing document. It covers the duties and ' +
             'responsibilities of officers, procedures for meetings and elections, and conduct guidelines for RPI ' +
-            'Ambulance Members. The current Constitution was ratified by the general membership on September 23, 2010, ' +
-            'and passed by the E-Board on October 23, 2014.'
+            'Ambulance Members. The current Constitution was ratified by the general membership on September 23, 2014, ' +
+            'and passed by the E-Board on October 23, 2014. \n <a href="../img/RPI_Ambulance_Constitution_UPDATED_OCTOBER_2014.pdf">Current Constitution</a>'
         }
     ];
 }]);
