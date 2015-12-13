d57fb2847c2b3bcb1a0d506817cf578fb314d369
David-Sparky <sparkd@rpi.edu>
2015-12-13 15:55:10 -0500

    fixes #10, fixes #16

--- public/js/controllers/SupervisorTrainingCtrl.js
+++ public/js/controllers/SupervisorTrainingCtrl.js
@@ -1,4 +1,54 @@
 var ctrl_name = 'SupervisorTrainingCtrl';
-angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
+angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function ($scope) {
+    $scope.pageHeader = 'Supervisor Training';
+    $scope.columns = false;
+    $scope.sections = [
+        {
+            header: 'Event EMS Supervisor (EES)',
+            internal_title: 'EES',
+            body: 'Event EMS Supervisors (EES) are full crew chiefs who have been specially trained to supervise RPI ' +
+            'Ambulance operations at special events and functions. The EES-in charge (unit 900) acts as a in-house ' +
+            'dispatcher and medical supervisor.'
+        },
 
+        {
+            header: 'EES Forms',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe frameborder="0" class="embed-responsive-item" style="width: 100% !important; height: 100% !important;"' +
+            'src="https://drive.google.com/embeddedfolderview?id=0B3mvXB0aR4DiejdzdG1TQlI1QWc#list"></iframe></div>'
+        },
+
+        {
+            header: 'Duty Supervisor',
+            internal_title: 'Duty Supervisor',
+            body: 'The Duty Supervisor is on call 24 hours a day to handle any emergencies, incidents, or other issues ' +
+            'that must be handled promptly, or to assist crews or members as needed. Duty Supervisors are the most ' +
+            'highly credentialed members in the agency. The on-duty Supervisor carries a dedicated cell phone (providing' +
+            ' members, DPS, other entities a way to always reach a knowledgeable person via phone – the cell phone is ' +
+            'always able to be answered, and the number will not change with changes in supervisors, unlike changes in ' +
+            'officers will). The supervisor also carries an RPIA portable and a portable 800, in order to be able to ' +
+            'monitor and contact the duty crew or the county via radio, if necessary. The Supervisor is Car 4.'
+
+
+        },
+        {
+            header: 'Duty Supervisor Requirements',
+            internal_title: 'DS Requirements',
+            body: 'To be a Duty Supervisor a member must satisfy the following requirements:' + '\n' +
+            '* Be a Crew Chief Trainer\n' +
+            '* Be a Driver Trainer\n' +
+            '* Be an EES\n' +
+            '* Have a 70% or higher QI compliance rate\n' +
+            '* Pass a promotional board consisting of the Captain and current Duty Supervisors'
+
+        },
+
+        {
+            header: 'Supervisor Forms',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe frameborder="0" class="embed-responsive-item" style="width: 100% !important; height: 100% !important;"' +
+            'src="https://drive.google.com/embeddedfolderview?id=0B3mvXB0aR4DiejdzdG1TQlI1QWc#list"></iframe></div>'
+        }
+
+    ];
 }]);
