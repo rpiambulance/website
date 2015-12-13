c2e441ab7884744c360b83a56b80f337a2df063d
David-Sparky <sparkd@rpi.edu>
2015-12-13 11:54:32 -0500

    In-service page is now angular and using GD

--- public/js/controllers/InServiceTrainingCtrl.js
+++ public/js/controllers/InServiceTrainingCtrl.js
@@ -1,4 +1,20 @@
-var ctrl_name = 'InServiceTrainingCtrl';
-angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
+angular.module('InServiceTrainingCtrl', []).controller('InServiceTrainingCtrl', ['$scope', function($scope) {
+    $scope.pageHeader = 'In Service Training';
+    $scope.columns = true;
 
+    $scope.sections = [
+        {
+            header: 'New York State DOH Forms',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe frameborder="0" class="embed-responsive-item" style="width: 100% !important; height: 100% !important;"' +
+            'src="https://drive.google.com/embeddedfolderview?id=0B3mvXB0aR4DiejdzdG1TQlI1QWc#list"></iframe></div>'
+        },
+        {
+            header: 'Agency-Specific Forms',
+            body: '<div class="embed-responsive embed-responsive-16by9"> ' +
+            '<iframe frameborder="0" class="embed-responsive-item" style="width: 100% !important; height: 100% !important;"' +
+            'src="https://drive.google.com/embeddedfolderview?id=0B3mvXB0aR4DiejdzdG1TQlI1QWc#list"></iframe></div>'
+        }
+
+    ];
 }]);
