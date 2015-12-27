955b652eb4fea76347f6c2f45f1373860c8a1e91
David-Sparky <sparkd@rpi.edu>
2015-12-27 14:26:35 -0800

    Added form for issues (GD)

--- public/js/controllers/ReportIssueCtrl.js
+++ public/js/controllers/ReportIssueCtrl.js
@@ -6,6 +6,12 @@ angular.module('ReportIssueCtrl', []).controller('ReportIssueCtrl', ['$scope', f
             internal_title: 'Message',
             body: 'We\'re so sorry something isn\'t working quite right. Please use the following form to tell us more' +
                 ' about the problem you\'ve just encountered.'
+        },
+
+        {
+            header: 'Issue Report',
+            internal_title: 'Form',
+            body: '<iframe src="https://docs.google.com/a/rpiambulance.com/forms/d/1aig3C6XMon3KXLMxtUgDpSbKqsx_tB-ZPQE4uhXj0i0/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
         }
 
     ];
