ec833d5075480e7872163d44e075550c6c247b70
David-Sparky <sparkd@rpi.edu>
2015-12-28 13:42:53 -0800

    Fixes #23, #24, #28; Added past oboards and fixed google maps

--- public/js/controllers/OfficersCtrl.js
+++ public/js/controllers/OfficersCtrl.js
@@ -15,34 +15,46 @@ angular.module('OfficersCtrl', []).controller('OfficersCtrl', ['$scope', '$sce',
             header: 'Officer Board 2015-2016',
             internal_title: 'O-Board Photo',
             type: 'img',
-            img: 'img/O-Board',
+            img: 'img/O-Board.jpg',
             width: '75%',
             alt: 'Photo of the office board',
             centered: true
         },
 
         {
+            header: 'Current Officer Board 2015-2016',
+            internal_title: 'Past Officer Boards',
+            body: 'Line Side: \n' +
+            '* Captain: Tom Manzini \n' +
+            '* First Lieutenant: Brittany Rupp \n' +
+            '* Second Lieutenant: Harrison Leinweber \n' +
+            '\nCivil Side:\n' +
+            '* President: Ciera Williams\n' +
+            '* Vice President: Rebbecca Martin'
+        },
+
+        {
             header: 'Officer Board Spring 2015',
             internal_title: 'Past Officer Boards',
             body: 'Line Side: \n' +
-            '*Captain: Tom Manzini ' +
-            '*First Lieutenant: Jordan Williams\n' +
-            '*Second Lieutenant: Brittany Rupp' +
-            'Civil Side:\n' +
-            '*Vice President: Rebecca Martin' +
-            '*President: Alexander Benzell'
+            '* Captain: Tom Manzini \n' +
+            '* First Lieutenant: Jordan Williams\n' +
+            '* Second Lieutenant: Brittany Rupp\n' +
+            '\nCivil Side:\n' +
+            '* President: Alexander Benzell\n' +
+            '* Vice President: Rebbecca Martin'
         },
 
         {
             header: 'Officer Board Fall 2014',
             internal_title: 'Past Officer Boards',
             body: 'Line Side: \n' +
-            '*Captain: Breanna Bernardi ' +
-            '*First Lieutenant: Drew Kopicki\n' +
-            '*Second Lieutenant: Jordan Williams' +
-            'Civil Side:\n' +
-            '*Vice President: Elise Romberger' +
-            '*President: Alli Morgan'
+            '* Captain: Breanna Bernardi \n' +
+            '* First Lieutenant: Drew Kopicki\n' +
+            '* Second Lieutenant: Jordan Williams\n' +
+            '\nCivil Side:\n' +
+            '* President: Alli Morgan\n' +
+            '* Vice President: Elise Romberger'
         }
 
 
