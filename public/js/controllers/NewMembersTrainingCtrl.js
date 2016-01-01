3c6068374243c43fc6d0aa83fd5d79aba505fe50
David-Sparky <sparkd@rpi.edu>
2016-01-01 09:28:14 -0500

    Formatting and grammar changes. Fixes #53, Fixes #54

--- public/js/controllers/NewMembersTrainingCtrl.js
+++ public/js/controllers/NewMembersTrainingCtrl.js
@@ -7,7 +7,7 @@ angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) 
             internal_title: 'Text',
             body: 'Members that are new to RPI Ambulance start out as observers. Observers do not take part in patient ' +
             'care, and are only there to observe and learn about the operations and the equipment on the ambulance. ' +
-            'he goal of all observers is to be promoted to Attendant. All New Members must go through a mandatory ' +
+            'The goal of all observers is to be promoted to Attendant. All New Members must go through a mandatory ' +
             'OSHA (Occupational Safety and Health Administration) safety training, a HIPAA (Health Insurance ' +
             'Portability and Accountability Act) patient confidentiality training and and RPI Ambulance Orientation Class.'
         },
