f3e2f9935665af91f83f7fbdf04ca5e8c6c36e33
David-Sparky <sparkd@rpi.edu>
2016-01-01 09:36:00 -0500

    Made requested changes to how to join page.

--- public/js/controllers/JoinCtrl.js
+++ public/js/controllers/JoinCtrl.js
@@ -10,13 +10,6 @@ angular.module('JoinCtrl', []).controller('JoinCtrl', ['$scope', function ($scop
             '\'upcoming events\' section on the home page. If you feel you will not be able to join us for a meeting, just' +
             'reach out to us using the <a href="#/contact">contact us</a> page and we\'ll figure something out.'
         },
-        {
-            header: 'Are the any requirements to join?',
-            body: 'None at all! All we ask is that you\'re willing to participate. Many of our members don\'t have any ' +
-            'prior experience in EMS and we do most of our own training. Unlike many other agencies we also don\'t require ' +
-            'you to be an EMT to ride with us. However, if you are interested in becoming an EMT, there is usually one ' +
-            'class per semester.'
-        },
 
         {
             header: 'I\'m not an RPI student. Can I still join?',
@@ -24,7 +17,17 @@ angular.module('JoinCtrl', []).controller('JoinCtrl', ['$scope', function ($scop
             'contact us</a> page to find out the best time to come to one of our meetings to introduce yourself. Since ' +
             'you are not an active student at RPI, members of the agency will have to vote you in as a new member during' +
             'one of the general body meetings.'
+        },
+
+        {
+            header: 'Are the any requirements to join?',
+            body: 'None at all! All we ask is that you\'re willing to participate. Many of our members don\'t have any ' +
+            'prior experience in EMS and we do most of our own training. Unlike many other agencies we also don\'t require ' +
+            'you to be an EMT to ride with us. However, if you are interested in becoming an EMT, there is usually one ' +
+            'class per semester.'
         }
+
+
 
     ];
 }]);
