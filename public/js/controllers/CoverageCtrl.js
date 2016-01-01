2c015da9081422a2f3b16a092bc091bbf7e9c64c
Justin Etzine <justetz@gmail.com>
2015-12-31 22:51:34 -0500

    Added debugs

--- public/js/controllers/CoverageCtrl.js
+++ public/js/controllers/CoverageCtrl.js
@@ -1,10 +1,10 @@
-angular.module('CoverageCtrl', []).controller('CoverageCtrl', ['$scope', '$http', function($scope, $http) {
+angular.module('CoverageCtrl', []).controller('CoverageCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.showModal = false;
     $scope.currentTier = "";
     $scope.submission = false;
     $scope.formData = {};
 
-    $scope.getAttendanceLims = function() {
+    $scope.getAttendanceLims = function () {
         return {
             "Tier I": "15-99",
             "Tier II": "100-4999",
@@ -12,37 +12,39 @@ angular.module('CoverageCtrl', []).controller('CoverageCtrl', ['$scope', '$http'
         }[$scope.currentTier];
     };
 
-    $scope.toggleModal = function(id) {
+    $scope.toggleModal = function (id) {
         $scope.currentTier = id;
         $scope.showModal = !$scope.showModal;
     };
 
-    var param = function(data) {
+    var param = function (data) {
         var returnString = '';
-        for (d in data){
+        for (var d in data) {
             if (data.hasOwnProperty(d))
                 returnString += d + '=' + data[d] + '&';
         }
         // Remove last ampersand and return
-        return returnString.slice( 0, returnString.length - 1 );
+        return returnString.slice(0, returnString.length - 1);
     };
 
-    $scope.clearForm = function() {
-        for (d in $scope.formData){
+    $scope.clearForm = function () {
+        for (var d in $scope.formData) {
             if (data.hasOwnProperty(d))
                 data[d] = "";
         }
     };
 
-    $scope.submitForm = function() {
+    $scope.submitForm = function () {
         $http({
-            method : 'POST',
-            url : '.email_submit.php',
-            data : param($scope.formData) + "&tier=" + $scope.currentTier, // pass in data as strings
-            headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+            method: 'POST',
+            url: '.email_submit.php',
+            data: param($scope.formData) + "&tier=" + $scope.currentTier, // pass in data as strings
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'} // set the headers so angular passing info as form data (not request payload)
         })
-            .success(function(data) {
+            .success(function (data) {
                 if (!data.success) {
+                    console.log("it failed!");
+                    console.log(data);
                     // if not successful, bind errors to error variables
                     $scope.errorName = data.errors.name;
                     $scope.errorEmail = data.errors.email;
@@ -50,6 +52,8 @@ angular.module('CoverageCtrl', []).controller('CoverageCtrl', ['$scope', '$http'
                     $scope.submissionMessage = data.messageError;
                     $scope.submission = true; //shows the error message
                 } else {
+                    console.log("it succeeded!");
+                    console.log(data);
                     // if successful, bind success message to message
                     $scope.submissionMessage = data.messageSuccess;
                     $scope.formData = {}; // form fields are emptied with this line
