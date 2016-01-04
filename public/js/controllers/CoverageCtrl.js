71244d367c1a164f3764f4d8bff81c387bfe1798
Justin Etzine <justetz@gmail.com>
2016-01-03 20:55:14 -0500

    Trying to fix emailing, as well as more debug print statements!

--- public/js/controllers/CoverageCtrl.js
+++ public/js/controllers/CoverageCtrl.js
@@ -36,6 +36,7 @@ angular.module('CoverageCtrl', []).controller('CoverageCtrl', ['$scope', '$http'
 
     $scope.submitForm = function () {
         console.log($scope.formData);
+        console.log(param($scope.formData) + "&tier=" + $scope.currentTier);
         $http({
             method: 'POST',
             url: '.email_submit.php',
