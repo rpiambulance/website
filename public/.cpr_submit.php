afac008c98c89df4c2077bc2c1f308cadbf85325
David-Sparky <sparkd@rpi.edu>
2016-01-20 10:47:57 -0800

    Working on CPR mail script

---
+++ public/.cpr_submit.php
@@ -0,0 +1,81 @@
+<?php
+
+$errors = array();
+
+// array to pass back data
+$data = array();
+
+// Get the input ===============================================================
+$formData = file_get_contents('php://input');
+$input = json_decode($formData, true);
+
+if (!isset($input['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (!isset($input['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (!isset($input['days'])) {
+    $errors['days'] = 'Please select which days work for you.';
+}
+
+if (!isset($input['type'])) {
+    $errors['type'] = 'Please select which type of training you would like.';
+}
+
+if (!empty($errors)) {
+    // if there are items in our errors array, return those errors
+    $data['success'] = false;
+    $data['errors'] = $errors;
+    $data['messageError'] = 'Please check the fields in red';
+} else {
+    // if there are no errors, return a message
+    $data['success'] = true;
+    $data['messageSuccess'] = 'Thanks for reaching out! We will get back to you soon as possible!';
+
+    //**************************
+    //******SET TEMP VARS*******
+    //**************************
+
+    $name       =   $input['name'];
+    $email      =   $input['email'];
+    $days    =   $input['days'];
+    $type    =   $input['type'];
+
+    //**************************
+    //********CONSTANTS*********
+    //**************************
+
+    $email_to="officers@rpiambulance.com";
+
+    // EMAIL TO THE O-BOARD ************************
+
+    $email_message = "A CPR training request was processed by the website. Details can be found below:\n" .
+        "\nName: " . $name .
+        "\nEmail: " . $email .
+        "\nDays Available: " . $days .
+        "\nTraining Type: " . $type;
+
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $subject, $email_message, $headers);
+
+    // Email the sender a confirmation:
+    $headers_requester= "From: no-reply-robots@rpiambulance.com \r\n".
+        'X-Mailer: PHP/' . phpversion();
+    $subject = "CPR Training Request";
+    $message = "Dear " . $name . ",\n\nThank you so much for reaching out to RPI Ambulance. We will respond to your " .
+        "request for CPR training as soon as we can. For your records, details of your request have in included below.
+        \n\nSincerely,\n\nThe RPI Ambulance Team" .
+
+        "\n\nDays Available: " . $days .
+        "\nTraining Type: " . $type;
+
+    @mail($email, $subject, $message, $headers_requester);
+}
+
+// return all our data to an AJAX call =========================================
+echo json_encode($data);
--- public/js/controllers/CPRCertificationCtrl.js
+++ public/js/controllers/CPRCertificationCtrl.js
@@ -1,4 +1,50 @@
 var ctrl_name = 'CPRCertificationCtrl';
 angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
 
+    var autocompleteValidate = function () {
+        var corrected = {};
+        for (var d in $scope.formData) {
+            if($scope.formData.hasOwnProperty(d)) {
+                if(document.getElementById(d).value !== $scope.formData[d]) {
+                    corrected[d] = document.getElementById(d).value;
+                } else {
+                    corrected[d] = $scope.formData[d];
+                }
+            }
+        }
+        return corrected;
+    };
+
+    $scope.clearForm = function () {
+        for (var d in $scope.formData) {
+            if ($scope.formData.hasOwnProperty(d))
+                $scope.formData[d] = "";
+        }
+    };
+
+    $scope.submitForm = function () {
+        $http({
+            method: 'POST',
+            url: '.cpr_submit.php',
+            data: autocompleteValidate(), // pass in data as strings
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'} // set the headers so angular passing info as form data (not request payload)
+        }).success(function (data) {
+            if (!data.success) {
+                console.log("it failed!");
+                // if not successful, bind errors to error variables
+                $scope.errorName = data.errors.name;
+                $scope.errorEmail = data.errors.email;
+                $scope.errorDays = data.errors.days;
+                $scope.errorType = data.errors.type;
+                $scope.submission = true; //shows the error message
+            } else {
+                $scope.showContactSuccess = true;
+                // if successful, bind success message to message
+                $scope.submissionMessage = data.messageSuccess;
+                $scope.formData = {}; // form fields are emptied with this line
+                $scope.submission = true; //shows the success message
+            }
+        });
+    };
+
 }]);
