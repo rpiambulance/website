970115925bc80c0fdfdf9843d0f751141bffbde0
David-Sparky <sparkd@rpi.edu>
2016-01-19 18:46:12 -0800

    Fixed copyright spelling and punctuation

--- public/js/controllers/CopyCtrl.js
+++ public/js/controllers/CopyCtrl.js
@@ -4,8 +4,8 @@ angular.module('CopyCtrl', []).controller('CopyCtrl', ['$scope', function($scope
         {
             header: 'Images',
             internal_title: 'Images',
-            body: 'All images on this site are either the property of RPI Ambulance or used with the explicit permission' +
-            'of their owner. If you would like to use one of our photos or have comments regarding them. please reach ' +
+            body: 'All images on this site are either the property of RPI Ambulance or used with the explicit permission ' +
+            'of their owner. If you would like to use one of our photos or have comments or concerns regarding them, please reach ' +
             'out to use using the contact us page and we will get back to you as soon as we can. No real patients are ' +
             'pictured in any of the images on our site.'
         },
@@ -13,10 +13,10 @@ angular.module('CopyCtrl', []).controller('CopyCtrl', ['$scope', function($scope
         {
             header: 'Site',
             internal_title: 'Site copy',
-            body: 'This website was a project completed the RPI Web Technologies Group working alongside with members of' +
-            'RPI Ambulance to make sure all needs wer met. This site was released under an open source license. Links to' +
+            body: 'This website was a project completed the RPI Web Technologies Group working alongside with members of ' +
+            'RPI Ambulance to make sure all needs were met. This site was released under an open source license. Links to ' +
             'the GitHub Repository can be found in the lower menu at the bottom of this page. If  you have any ' +
-            'questions please feel free to reach out to the development team or our webmaster.'
+            'questions or concerns please feel free to reach out to the development team or our webmaster.'
         }
 
         ];
