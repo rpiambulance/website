76d57bd17a08aec7f539ec83dd58b83885126d4f
David-Sparky <sparkd@rpi.edu>
2016-01-20 18:11:07 -0800

    Hunted down and destroyed isotope

--- public/js/plugin-config.js
+++ public/js/plugin-config.js
@@ -15,23 +15,6 @@ jQuery(function($) {'use strict',
 	//Initiat WOW JS
 	new WOW().init();
 
-	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
 
 	// Contact form
 	var form = $('#main-contact-form');
