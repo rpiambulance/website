17b4f547aa2bf8e8877c57beaf71ca84d40e83d7
David-Sparky <sparkd@rpi.edu>
2016-01-21 20:00:54 -0800

    Deleted things

--- public/js/controllers/fuelLogCtrl.js
+++ public/js/controllers/fuelLogCtrl.js
@@ -1,3 +1,39 @@
-angular.module('FuelLogCtrl', []).controller('FuelLogCtrl', ['$scope', function($scope) {
-    //Remains empty
+angular.module('FuelLogCtrl', []).controller('FuelLogCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.formData = {
+        name: "",
+        email: ""
+    };
+
+    var autocompleteValidate = function () {
+        var corrected = {};
+        for (var d in $scope.formData) {
+            if($scope.formData.hasOwnProperty(d)) {
+                if(document.getElementById(d).value !== $scope.formData[d]) {
+                    corrected[d] = document.getElementById(d).value;
+                } else {
+                    corrected[d] = $scope.formData[d];
+                }
+            }
+        }
+        return corrected;
+    };
+
+    $scope.clearForm = function () {
+        for (var d in $scope.formData) {
+            if ($scope.formData.hasOwnProperty(d))
+                $scope.formData[d] = "";
+        }
+    };
+
+    $scope.vehicle = {
+        Amb: false,
+        FR59: false
+    };
+
+
+    $scope.getDatetime = new Date();
+
 }]);
+
+
