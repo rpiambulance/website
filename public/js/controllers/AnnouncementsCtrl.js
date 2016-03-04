2613693cd47071fdb477259b8ecb566fceb5a924
David-Sparky <sparkd@rpi.edu>
2016-03-04 12:31:45 -0500

    Added even more things. Yes I know 3 things are still broken

--- public/js/controllers/AnnouncementsCtrl.js
+++ public/js/controllers/AnnouncementsCtrl.js
@@ -1,3 +1,14 @@
-angular.module('AnnouncementsCtrl', []).controller('AnnouncementsCtrl', ['$scope', '$http', function($scope, $http) {
+var ctrl_name = 'AnnouncementsCtrl';
+angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function ($scope) {
+    $scope.pageHeader = 'Announcements';
+    $scope.columns = false;
+    $scope.sections = [
+        {
+            header: 'FREE PIE',
+            internal_title: 'FREE PIE',
+            body: 'Hi Everyone, \n\n I as President of the agency declare that everyone shall give Sparky free pie on ' +
+            'pi day. That is all. \n\n &mdash; Ciera'
+        }
 
+    ];
 }]);
