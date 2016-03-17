c492b1b0fece1a0358d2e9765f1665a5cf0c6476
David-Sparky <sparkd@rpi.edu>
2016-03-16 18:57:30 -0600

    Messing with http.get

--- public/.crews.php
+++ public/.crews.php
@@ -4,21 +4,23 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require_once ".db_config.php";
 
-$connection = new PDO($dsn, $duser, $dpassword);
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connection = new mysqli($dsn, $duser, $dpassword);
+//$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Selecting Database
 //$db = mysql_select_db("$db_name", $connection);
-$connection->exec("USE `$db_name`");
+$connection->query("USE `$db_name`");
 
-$result = $connection->query("SELECT CompanyName, City, Country FROM Customers");
+$result = $connection->query("SELECT date, cc, driver, attendant, observer FROM crews");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
-    $outp .= '"City":"'   . $rs["City"]        . '",';
-    $outp .= '"Country":"'. $rs["Country"]     . '"}';
+    $outp .= '{"date":"'  . $rs["date"] . '",';
+    $outp .= '"cc":"'   . $rs["cc"]        . '",';
+    $outp .= '"driver":"'   . $rs["driver"]        . '",';
+    $outp .= '"attendant":"'   . $rs["attendant"]        . '",';
+    $outp .= '"observer":"'. $rs["observer"]     . '"}';
 }
 $outp ='{"records":['.$outp.']}';
 $connection->close();
--- public/js/controllers/NightCrewsCtrl.js
+++ public/js/controllers/NightCrewsCtrl.js
@@ -3,6 +3,10 @@ angular.module('NightCrewsCtrl', []).controller('NightCrewsCtrl', ['$scope', '$h
     $scope.number= 7;
     $scope.getNumber = function(num) {
         return new Array(num);
-    }
+    };
+
+
+    $http.get("http://127.0.0.1:63342")
+        .then(function (response) {$scope.names = response.data.records;});
 
 }]);
