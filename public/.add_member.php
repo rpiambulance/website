c45a67d7d4689da5663eb7cb40b6ddf0a1f46558
David-Sparky <sparkd@rpi.edu>
2016-03-26 19:23:08 -0400

    Add_member still in progress

--- public/.add_member.php
+++ public/.add_member.php
@@ -2,6 +2,33 @@
 
 require_once ".db_config.php";
 
+// empty response
+$response = null;
+
+//array to hold errors
+$errors = array();
+
+// array to pass back data
+$data = array();
+
+// Get the input ===============================================================
+$formData = file_get_contents('php://input');
+$input = json_decode($formData, true);
+
+$first_name = $input['first_name'];
+$last_name = $input['last_name'];
+$password = $input['password'];
+$password= md5($password);
+$email = $input['email'];
+$rcs = $input['RCS'];
+$rin = $input['RIN'];
+$home_phone= $input['phone'];
+$cell_phone= $input['c_phone'];
+$rpi_address= $input['rpi_add'];
+$home_address= $input['home_add'];
+$dob= $input['dob'];
+
+
 $connection = new PDO($dsn, $duser, $dpassword);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -9,13 +36,12 @@ $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //$db = mysql_select_db("$db_name", $connection);
 $connection->exec("USE `$db_name`");
 
-$statement=$connection->prepare("SELECT * FROM members WHERE username = '$username'");
+$statement = $connection->prepare("SELECT * FROM members WHERE username = '$username'");
 
 $usernamecheck = mysql_num_rows($statement);
-if($usernamecheck == 0)
-{
+if ($usernamecheck == 0) {
     $highID = $connection->exec("SELECT MAX(id) FROM members");
-    $logid = $highID+1;
+    $logid = $highID + 1;
     $connection->exec("INSERT INTO members (id, username, password, first_name, last_name, dob, email, rcs_id, rin, rpi_address, home_address, cell_phone, home_phone) VALUES ($logid, '$username', '$password', '$first_name', '$last_name', '$dob', '$email', '$rcs_id', $rin, '$rpi_address', '$home_address', '$cell_phone', '$home_phone')");
 }
 
--- public/js/controllers/AddMemberCtrl.js
+++ public/js/controllers/AddMemberCtrl.js
@@ -1,3 +1,77 @@
 angular.module('AddMemberCtrl', []).controller('AddMemberCtrl', ['$scope', '$http', function($scope, $http) {
 
+
+    $scope.formData = {
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        conf_password: "",
+        RCS: "",
+        RIN: "",
+        phone: "",
+        c_phone: "",
+        rpi_add: "",
+        h_add: "",
+        dob: ""
+    };
+
+    var autocompleteValidate = function () {
+        var corrected = {};
+        for (var d in $scope.formData) {
+            if($scope.formData.hasOwnProperty(d)) {
+                if(document.getElementById(d).value !== $scope.formData[d]) {
+                    corrected[d] = document.getElementById(d).value;
+                } else {
+                    corrected[d] = $scope.formData[d];
+                }
+            }
+        }
+        return corrected;
+    };
+
+    $scope.clearForm = function () {
+        for (var d in $scope.formData) {
+            if ($scope.formData.hasOwnProperty(d))
+                $scope.formData[d] = "";
+        }
+    };
+
+    $scope.submitForm = function () {
+
+        $http({
+            method: 'POST',
+            url: '.add_member.php',
+            data: autocompleteValidate(), // pass in data as strings
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'} // set the headers so angular passing info as form data (not request payload)
+        }).success(function (data) {
+            if (!data.success) {
+                console.log("it failed!");
+                // if not successful, bind errors to error variables
+                if(data.errors.name) {
+                    $scope.errorName = data.errors.name;
+                }
+                if(data.errors.email) {
+                    $scope.errorEmail = data.errors.email;
+                }
+                if(data.errors.message) {
+                    $scope.errorTextarea = data.errors.message;
+                }
+                if(data.messageError) {
+                    $scope.submissionMessage = data.messageError;
+                }
+
+                $scope.submission = true; //shows the error message
+            } else {
+                $scope.showContactSuccess = true;
+                // if successful, bind success message to message
+                $scope.submissionMessage = data.messageSuccess;
+                $scope.formData = {}; // form fields are emptied with this line
+                $scope.submission = true; //shows the success message
+            }
+        });
+    };
+
+
+
 }]);
