15268de42ce7c6e6170d2c8159a494665375d00f
Justin Etzine <justetz@gmail.com>
2016-08-06 10:21:57 -0700

    Forgot to pull before coding

--- public/.login.php
+++ public/.login.php
@@ -7,24 +7,20 @@ require_once ".db_config.php";
 $response = null;
 
 $errors = array();
-
-// array to pass back data
 $data = array();
 
-// Get the input ===============================================================
-$formData = file_get_contents('php://input');
-$input = json_decode($formData, true);
+$support_email = 'webmaster@rpiambulance.com';
 
 // if submitted check response
 
 if ($response == null) {
 }
 
-if (!isset($input['username'])) {
+if (!isset($_POST['username'])) {
     $errors['username'] = 'Username is required.';
 }
 
-if (!isset($input['password'])) {
+if (!isset($_POST['password'])) {
     $errors['pass'] = 'Password is required.';
 }
 
@@ -32,18 +28,10 @@ if (!empty($errors)) {
     // if there are items in our errors array, return those errors
     $data['success'] = false;
     $data['errors'] = $errors;
-    $data['messageError'] = 'Please check the fields in red';
 } else {
-    // if there are no errors, return a message
-    $data['success'] = true;
-    $data['messageSuccess'] = 'Logging In!';
 
-    //**************************
-    //******SET TEMP VARS*******
-    //**************************
-
-    $username  =   $input['username'];
-    $pass      =   $input['password'];
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 
     $connection = new PDO("mysql:host=$dhost;dbname=$dname", $duser, $dpassword);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -55,34 +43,49 @@ if (!empty($errors)) {
 
     $pass = md5($pass);
 
-    // SQL query to fetch information of registerd users and finds user match.
-    //$query = mysql_query("SELECT * FROM login WHERE password='$password' AND username='$username'", $connection);
-    //$rows = mysql_num_rows($query);
-    $sql="SELECT * FROM members WHERE username=:username AND password=:password";
-    $stmt=$connection->prepare($sql);
-    $stmt->bindParam(':password', $password);
-    $stmt->bindParam(':username', $username);
-    $didSucceed = $stmt->execute();
-    $rows=$stmt->fetch();
-    $num= $rows->rowCount();
+    $statement=$connection->prepare("SELECT * FROM members WHERE username=:username AND password=:password");
+    $statement->bindParam(':password', $pass);
+    $statement->bindParam(':username', $user);
+    $statement->execute();
+    $rows=$statement->fetch(PDO::FETCH_ASSOC);
+
+
+
     //GET ACTIVE MEMBER STATUS SINCE ONLY ACTIVE MEMBERS CAN SIGN IN
     //echo('Rows' .$rows);
 
-    if ($didSucceed && $num == 1) {
-        // Initializing Session
-        session_start();
-        // $_SESSION['name'] =
-        $_SESSION['username'] = $username;
+    if (!empty($user)) {
+        if($user['active'] != '1') {
+            $errors['inactive'] = 'Your account is not active. Please contact ' . $support_email . ' for assistance.';
+            $data['success'] = false;
+            $data['errors'] = $errors;
+        } else if($user['access_revoked'] == '1') {
+            $errors['inactive'] = 'Access has been revoked for your account. Please contact ' . $support_email . ' if you believe this was done in error.';
+            $data['success'] = false;
+            $data['errors'] = $errors;
+        } else {
+            $statement = $connection->prepare("UPDATE members SET lastlogin=CURDATE() WHERE id = :id");
+            $statement->prepare(':id', $user['id']);
+            $statement->execute();
 
-        $data['session_id'] = session_id();
-        //header("location:#/night-crews"); // Redirecting To Other Page
-    }
-    else{
-        echo("Wrong");
+            session_start();
+            // $_SESSION['name'] =
+            $_SESSION['username'] = $username;
+
+            $data['session_id'] = session_id();
+        }
+
+
+
+
+
+        echo json_encode($rows);
+    } else {
+        $errors['inactive'] = 'Your username and/or password are not correct. Please contact ' . $support_email . ' for assistance.';
+        $data['success'] = false;
+        $data['errors'] = $errors;
     }
 
     $connection= null;
     echo json_encode($data);
-
-
 }
