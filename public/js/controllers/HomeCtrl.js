fc62d8df99fbe74faa3f44f27cd0b18feaefcf94
Justin Etzine <justetz@gmail.com>
2016-08-09 14:34:46 -0400

    Using angular and jquery to direct user to correct page when clicking Read More on slider; closes #76

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,3 +1,6 @@
-angular.module('HomeCtrl', []).controller('HomeCtrl', function() {
-
-});
+angular.module('HomeCtrl', []).controller('HomeCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.readMore = function () {
+        $('#main-slider').carousel('pause');
+        $location.path('/' + $('.item.active').attr('id'));
+    }
+}]);
