18ae2b38a783c3e37307e045f5b512dd8b9a9a10
Justin Etzine <justetz@gmail.com>
2016-08-09 13:58:49 -0400

    Corrected elections month; resolves #77

--- public/js/controllers/OfficersCtrl.js
+++ public/js/controllers/OfficersCtrl.js
@@ -4,7 +4,7 @@ angular.module('OfficersCtrl', []).controller('OfficersCtrl', ['$scope', '$sce',
         {
             header: '',
             internal_title: 'Officers Description',
-            body: 'The officers of RPI Ambulance are elected at the first general body meeting of November. The ' +
+            body: 'The officers of RPI Ambulance are elected at the first general body meeting of April. The ' +
             'officers perform day to day and long term operational and administrative tasks. In 2009 the RPI Ambulance ' +
             'Constitution was changed to reduce the officer board from 8 officers to 5 consolidating "non-steering" ' +
             'positions into more important roles. This will allow for a smaller, more efficient officer board and the ' +
