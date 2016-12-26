db6672299bbd2e6ba6d141899b7ff668c5d295d7
Justin Etzine <justetz@gmail.com>
2016-12-25 21:45:07 -0500

    Doesn't work....yet

--- public/.crews.php
+++ public/.crews.php
@@ -8,7 +8,7 @@ $connection = new PDO("mysql:host=$dhost;dbname=$dname", $duser, $dpassword);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(!isset($dname)) {
-  $dname = 'ambulanc_web';
+    $dname = 'ambulanc_web';
 }
 
 // Selecting Database
@@ -20,35 +20,38 @@ $connection->exec("USE `$dname`");
 if(date("D") == "Sun") {
     $oneweek = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")+7, date("Y")));
 
-    $statement = $connection->prepare("SELECT COUNT(id) FROM crews WHERE date = :oneweek");
+    $statement = $connection->prepare("SELECT COUNT(id) FROM $dname.crews WHERE date = :oneweek");
     $statement->bindParam(":oneweek", $oneweek);
     $statement->execute();
     $count = $statement->fetchAll(PDO::FETCH_COLUMN, 0)[0];
 
-
     if($count < 1) {
         for($i = 7; $i < 14; $i++) {
-            $idarray = mysql_fetch_array(mysql_query("SELECT id FROM crews ORDER BY id DESC LIMIT 1")) or die(mysql_error());
-            $logid = $idarray['id']+1;
-            $default = mysql_fetch_array(mysql_query("SELECT * FROM default_crews WHERE day = $i-7"));
-            $date = date('Y-m-d', mktime(0, 0, 0, date('m')  , date('d')+$i, date('Y')));
-            $cc = $default['cc'];
-            $driver = $default['driver'];
-            $attendant = $default['attendant'];
-            $observer = $default['observer'];
-            mysql_query("INSERT INTO crews (id, date, cc, driver, attendant, observer) VALUES ($logid, '$date', $cc, $driver, $attendant, $observer)") or die(mysql_error());
+            $statement = $connection->prepare("SELECT id FROM crews ORDER BY id DESC LIMIT 1");
+            $statement->bindParam(":oneweek", $oneweek);
+            $statement->execute();
+            $logid = $statement->fetchColumn();
+
+        //
+        //     $default = mysql_fetch_array(mysql_query("SELECT * FROM default_crews WHERE day = $i-7"));
+        //     $date = date('Y-m-d', mktime(0, 0, 0, date('m')  , date('d')+$i, date('Y')));
+        //     $cc = $default['cc'];
+        //     $driver = $default['driver'];
+        //     $attendant = $default['attendant'];
+        //     $observer = $default['observer'];
+        //     mysql_query("INSERT INTO crews (id, date, cc, driver, attendant, observer) VALUES ($logid, '$date', $cc, $driver, $attendant, $observer)") or die(mysql_error());
         }
     }
 }
 
 try {
-  $statement=$connection->prepare("SELECT * FROM $dname.crews");
-  $statement->execute();
-  $results=$statement->fetchAll(PDO::FETCH_ASSOC);
+    $statement=$connection->prepare("SELECT * FROM $dname.crews ORDER BY id DESC LIMIT 14");
+    $statement->execute();
+    $results=$statement->fetchAll(PDO::FETCH_ASSOC);
 
-  echo json_encode(array("result" => $results, "success" => true));
+    echo json_encode(array("result" => $results, "success" => true));
 } catch (Exception $e) {
-  echo json_encode(array("error" => $e, "success" => false));
+    echo json_encode(array("error" => $e, "success" => false));
 }
 
 // for ($x = 0; $x <= sizeof($results); $x++) {
