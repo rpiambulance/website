bb61653b3f67b19341f6de8d5d789049a4944fec
Justin Etzine <justetz@gmail.com>
2016-12-25 23:00:38 -0500

    Display working for night crews

--- public/.crews.php
+++ public/.crews.php
@@ -4,7 +4,7 @@
 
 require_once ".db_config.php";
 
-$connection = new PDO("mysql:host=$dhost;dbname=$dname", $duser, $dpassword);
+$connection = new PDO("mysql:host=$dhost:3306;dbname=$dname", $duser, $dpassword);
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(!isset($dname)) {
@@ -15,37 +15,89 @@ if(!isset($dname)) {
 //$db = mysql_select_db("$dname", $connection);
 $connection->exec("USE `$dname`");
 
+$oneWeekOffset = 7 - date("N");
+$twoWeekOffset = 14 - date("N");
 
+if($oneWeekOffset == 0) { // Sunday
+    $oneWeekOffset = 7;
+    $twoWeekOffset = 14;
+}
 
-if(date("D") == "Sun") {
-    $oneweek = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d")+7, date("Y")));
+$oneweek = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d") + $oneWeekOffset, date("Y")));
+$twoweek = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d") + $twoWeekOffset, date("Y")));
 
-    $statement = $connection->prepare("SELECT COUNT(id) FROM $dname.crews WHERE date = :oneweek");
-    $statement->bindParam(":oneweek", $oneweek);
-    $statement->execute();
-    $count = $statement->fetchAll(PDO::FETCH_COLUMN, 0)[0];
+$statement = $connection->prepare("SELECT COUNT(id) FROM $dname.crews WHERE date = :oneweek");
+$statement->bindParam(":oneweek", $oneweek);
+$statement->execute();
+$oneWeekCount = $statement->fetchAll(PDO::FETCH_COLUMN, 0)[0];
 
-    if($count < 1) {
-        for($i = 7; $i < 14; $i++) {
-            $statement = $connection->prepare("SELECT id FROM crews ORDER BY id DESC LIMIT 1");
-            $statement->bindParam(":oneweek", $oneweek);
-            $statement->execute();
-            $logid = $statement->fetchColumn();
+$statement = $connection->prepare("SELECT COUNT(id) FROM $dname.crews WHERE date = :twoweek");
+$statement->bindParam(":twoweek", $twoweek);
+$statement->execute();
+$twoWeekCount = $statement->fetchAll(PDO::FETCH_COLUMN, 0)[0];
 
-        //
-        //     $default = mysql_fetch_array(mysql_query("SELECT * FROM default_crews WHERE day = $i-7"));
-        //     $date = date('Y-m-d', mktime(0, 0, 0, date('m')  , date('d')+$i, date('Y')));
-        //     $cc = $default['cc'];
-        //     $driver = $default['driver'];
-        //     $attendant = $default['attendant'];
-        //     $observer = $default['observer'];
-        //     mysql_query("INSERT INTO crews (id, date, cc, driver, attendant, observer) VALUES ($logid, '$date', $cc, $driver, $attendant, $observer)") or die(mysql_error());
-        }
+if($oneWeekCount < 1 || $twoWeekCount < 1) {
+    $start = 7;
+    $end = 7;
+    if($oneWeekCount < 1) {
+        $start = 0;
+    }
+    if($twoWeekCount < 1) {
+        $end = 14;
+    }
+
+    for($i = $start; $i < $end; $i++) {
+        $statement = $connection->prepare("SELECT id FROM crews ORDER BY id DESC LIMIT 1");
+        $statement->execute();
+        $logid = $statement->fetchColumn() + 1;
+
+        $statement = $connection->prepare("SELECT * FROM default_crews WHERE day = :day");
+        $statement->bindValue(":day", ($i % 7));
+        $statement->execute();
+        $defaultCrew = $statement->fetchAll(PDO::FETCH_ASSOC)[0];
+
+        $newDate = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d') + $i, date('Y')));
+
+        $statement = $connection->prepare("INSERT INTO `crews` (`id`, `date`, `cc`, `driver`, `attendant`, `observer`) VALUES (:logid, :newDate, :cc, :driver, :attendant, :observer)");
+        $statement->bindParam(":logid", $logid);
+        $statement->bindParam(":newDate", $newDate);
+        $statement->bindParam(":cc", $defaultCrew['cc']);
+        $statement->bindParam(":driver", $defaultCrew['driver']);
+        $statement->bindParam(":attendant", $defaultCrew['attendant']);
+        $statement->bindParam(":observer", $defaultCrew['observer']);
+        $statement->execute();
     }
 }
 
 try {
-    $statement=$connection->prepare("SELECT * FROM $dname.crews ORDER BY id DESC LIMIT 14");
+    $sql = <<<SQL
+        SELECT
+        	c.*,
+            cc.first_name AS cc_first_name,
+            cc.last_name AS cc_last_name,
+            driver.first_name AS driver_first_name,
+            driver.last_name AS driver_last_name,
+            attendant.first_name AS attendant_first_name,
+            attendant.last_name AS attendant_last_name,
+            observer.first_name AS observer_first_name,
+            observer.last_name AS observer_last_name
+        FROM
+        	crews c,
+            members cc,
+            members driver,
+            members attendant,
+            members observer
+        WHERE
+        	c.cc = cc.id AND
+            c.driver = driver.id AND
+            c.attendant = attendant.id AND
+            c.observer = observer.id
+        ORDER BY
+        	id DESC
+        LIMIT 14;
+SQL;
+
+    $statement=$connection->prepare($sql);
     $statement->execute();
     $results=$statement->fetchAll(PDO::FETCH_ASSOC);
 
--- public/js/controllers/NightCrewsCtrl.js
+++ public/js/controllers/NightCrewsCtrl.js
@@ -12,6 +12,31 @@ angular.module('NightCrewsCtrl', []).controller('NightCrewsCtrl', ['$scope', '$h
 
     $scope.currentWeekCrews = [];
     $scope.upcomingWeekCrews = [];
+
+    $http({
+        method: 'GET',
+        url: '.crews.php',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        // set the headers so angular passing info as form data (not request payload)
+    }).then(function (response) {
+        console.log('we here', response);
+        if (!response.data.success) {
+            console.log(response.data);
+            console.log("it failed!");
+            // if not successful, bind errors to error variables
+
+            $scope.submission = true; //shows the error message
+        } else {
+            // Since the crews come in order of latest first, we need to separate
+            // them in a way that almost seems backwards.
+
+            console.log(response.data.result);
+
+            $scope.currentWeekCrews = response.data.result.slice(7);
+            $scope.upcomingWeekCrews = response.data.result.slice(0, 7);
+        }
+    });
+
     $scope.number = 7;
     $scope.getNumber = function (num) {
         return new Array(num);
@@ -23,27 +48,6 @@ angular.module('NightCrewsCtrl', []).controller('NightCrewsCtrl', ['$scope', '$h
                 $scope.formData[d] = "";
         }
     };
-
-    $http({
-        method: 'POST',
-        url: '.crews.php',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        // set the headers so angular passing info as form data (not request payload)
-    }).success(function (data) {
-        if (!data.success) {
-            console.log(data);
-            console.log("it failed!");
-            // if not successful, bind errors to error variables
-
-            $scope.submission = true; //shows the error message
-        } else {
-            // Since the crews come in order of latest first, we need to separate
-            // them in a way that almost seems backwards.
-
-            $scope.currentWeekCrews = data.result.slice(7);
-            $scope.upcomingWeekCrews = data.result.slice(0, 7);
-        }
-    });
 
     $scope.formatName = function (first, last) {
         return first ? (first.substr(0,1) + '. ' + last) : (last ? last : '');
