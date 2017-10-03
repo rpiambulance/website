f45186baa05c15738aa81c4f25b41b2b1683b668
Dan Bruce <dan@ddbruce.com>
2017-10-03 14:53:06 -0400

    fixed <EMPTY> checking...now last name instead of first name

--- slack-whoson.php
+++ slack-whoson.php
@@ -1,7 +1,7 @@
 <?php
 
 function cleanName($name) {
-  if ($name[0]["first_name"] == "") {
+  if ($name[0]["last_name"] == "") {
     return "<EMPTY>";
   }
   return substr($name[0]["first_name"],0,1) . "." . " " . $name[0]["last_name"];
