fe6795921efbfda3b5eeef6c32d3eaa0972b82bd
Dan Bruce <dan@ddbruce.com>
2018-01-23 12:12:31 -0500

    added OOS functionality, moved DB query to function

--- slack-whoson.php
+++ slack-whoson.php
@@ -1,6 +1,9 @@
 <?php
 
 function cleanName($name) {
+  if ($name[0]["last_name"] == "OUT OF SERVICE") {
+    return "OOS";
+  }
   if ($name[0]["first_name"] == "") {
     if ($name[0]["last_name"] != "") {
       return $name[0]["last_name"];
@@ -8,6 +11,28 @@ function cleanName($name) {
     return "<EMPTY>";
   }
   return substr($name[0]["first_name"],0,1) . "." . " " . $name[0]["last_name"];
+}
+
+function getCrew($date) {
+  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT cc FROM crews WHERE date = '$date')");
+  $statement->execute();
+  $cc = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT driver FROM crews WHERE date = '$date')");
+  $statement->execute();
+  $driver = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT attendant FROM crews WHERE date = '$date')");
+  $statement->execute();
+  $attendant1 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT observer FROM crews WHERE date = '$date')");
+  $statement->execute();
+  $attendant2 = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  return (cleanName($cc) == "OOS") ? "OUT OF SERVICE" : "Crew chief: " . cleanName($cc) . "\n" .
+  "Driver: " . cleanName($driver) . "\n" .
+  "Attendants: " . cleanName($attendant1) . " and " . cleanName($attendant2);
 }
 
 require_once ".db_config.php";
@@ -30,45 +55,9 @@ try {
   $date = date("Y-m-d");
   $yesterday = date("Y-m-d", time() - 60 * 60 * 24);
 
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT cc FROM crews WHERE date = '$date')");
-  $statement->execute();
-  $cc = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $today_crew = getCrew($today);
 
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT driver FROM crews WHERE date = '$date')");
-  $statement->execute();
-  $driver = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT attendant FROM crews WHERE date = '$date')");
-  $statement->execute();
-  $attendant1 = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT observer FROM crews WHERE date = '$date')");
-  $statement->execute();
-  $attendant2 = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $today_crew = "Crew chief: " . cleanName($cc) . "\n" .
-  "Driver: " . cleanName($driver) . "\n" .
-  "Attendants: " . cleanName($attendant1) . " and " . cleanName($attendant2);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT cc FROM crews WHERE date = '$yesterday')");
-  $statement->execute();
-  $cc = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT driver FROM crews WHERE date = '$yesterday')");
-  $statement->execute();
-  $driver = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT attendant FROM crews WHERE date = '$yesterday')");
-  $statement->execute();
-  $attendant1 = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $statement = $connection->query("SELECT first_name, last_name FROM members WHERE id = (SELECT observer FROM crews WHERE date = '$yesterday')");
-  $statement->execute();
-  $attendant2 = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-  $yesterday_crew = "Crew chief: " . cleanName($cc) . "\n" .
-  "Driver: " . cleanName($driver) . "\n" .
-  "Attendants: " . cleanName($attendant1) . " and " . cleanName($attendant2);
+  $yesterday_crew = getCrew($yesterday);
 
   $now = new Datetime("now");
   $shiftstart = new DateTime('18:00');
