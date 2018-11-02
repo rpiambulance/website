269da2d39c379076382d5026cc6fb1c2717ca04e
Logan Ramos <lramos15@gmail.com>
2018-11-01 22:43:40 -0400

    More secure sessions and fixed logout (#76)

--- .logout.php
+++ .logout.php
@@ -1,8 +1,10 @@
 <?php
-
+require_once ".db_config.php";
 if(!isset($_GET['session_id'])) {
     http_response_code(400);
 } else {
+    $connection = new PDO("mysql:host=$dhost;dbname=$dname", $duser, $dpassword);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $connection->prepare('DELETE FROM sessions WHERE sessionID=:sessionID');
     $stmt->bindParam(":sessionID",$_GET['session_id']);
     $stmt->execute();
