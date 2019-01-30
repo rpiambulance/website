02e46e52a744a9772abb83d94f318c697b457687
Logan Ramos <lramos15@gmail.com>
2019-01-29 21:58:10 -0500

    Now supports member info command

--- slack-link.php
+++ slack-link.php
@@ -5,6 +5,83 @@ if(is_null($conn)){
     echo "Database connection failed to initialize!";
     return;
 }
+if(isset($_GET['slack_id']) && isset($_GET['type'])){
+    if($_GET['type'] != "info"){
+        return;
+    }
+    $statement = $conn->prepare("SELECT * FROM members WHERE slackID = :slack");
+    $statement->bindParam(":slack", $_GET['slack_id']);
+    $statement->execute();
+    $accounts = $statement->fetchAll();
+    if(!$accounts){
+        echo "No website accounts are associated with this ID!";
+        return;
+    }else{
+        foreach($accounts as $account){
+            $message = "";
+            // I need to move this to a separate function
+            if($account['captain'] == 1){
+                $message .= "*Captain*\n";
+            }else if($account['firstlt'] == 1){
+                $message .= "*First Lieutenant*\n";
+            }else if($account['secondlt'] == 1){
+                $message .= "*Second Lieutenant*\n";
+            }else if($account['pres'] == 1){
+                $message .= "*President*\n";
+            }else if($account['vicepres'] == 1){
+                $message .= "*Vice President*\n";
+            }else if($account['schedco'] == 1){
+                $message .= "*Scheduling Coordinator*\n";
+            }else if($account['traincommchair'] == 1){
+                $message .= "*Training Committee Chair*\n";
+            }else if($account['radioco'] == 1){
+                $message .= "*Radio Coordnator*\n";
+            }else if($account['cprco'] == 1){
+                $message .= "*CPR Coordinator*\n";
+            }else if($account['qaco'] == 1){
+                $message .= "*Resident Old Man*\n";
+            }
+            $message .= "Name: " . $account['first_name'] . " " . $account['last_name'];
+            // They have a radio number
+            if($account['radionum'] != 0){
+                $message .= " (" . $account['radionum'] . ")"; 
+            }
+            $message .= "\n";
+            $message .= "Positions:";
+            // This also needs to go into another functions
+            if($account['dutysup'] == 1){
+                $message .= " Duty Supervisor";
+                echo $message;
+                return;
+            }else{
+                if($account['ees'] == 1){
+                    $message .= " EES,";
+                }
+                if($account['cctrainer'] == 1){
+                    $message .= " CC-T,";
+                }else if($account['crewchief'] == 1){
+                    $message .= " CC,";
+                }else if($account['backupcc'] == 1){
+                    $message .= " P-CC,";
+                }
+                if($account['firstresponcecc'] == 1){
+                    $message .= " FR-CC,";
+                }
+                if($account['drivertrainer'] == 1){
+                    $message .= " D-T,";
+                }else if($account['driver'] == 1){
+                    $message .= " D,";
+                }else if($account['backupdriver'] == 1){
+                    $message .= " P-D";
+                }
+            }
+            $message = rtrim($message, ',');
+            $message .= "\n";
+            echo $message;
+        }
+        return;
+    }
+}
 if (isset($_GET['slack_id'])){
     $statement = $conn->prepare("SELECT id, first_name, last_name FROM members WHERE slackID = :slack");
     $statement->bindParam(":slack", $_GET['slack_id']);
@@ -37,5 +114,6 @@ if(isset($_POST["slack_id"]) && isset($_POST['member_id'])){
     echo "Successfully linked " . $_POST['slack_id'] . " to " . $user['first_name'] . " " . $user['last_name'] . " (" . $_POST['member_id'] . ")";
 }else{
     echo "Invalid request";
+    return;
 }
 ?>
