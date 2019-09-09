dd8fb6a8d9b088ad42bc72650f05c1b53567edf0
Dan Bruce <ddbruce@users.noreply.github.com>
2019-09-08 21:44:00 -0400

    Update OfficersCtrl.js

--- js/controllers/OfficersCtrl.js
+++ js/controllers/OfficersCtrl.js
@@ -4,8 +4,8 @@ angular.module('OfficersCtrl', []).controller('OfficersCtrl', ['$scope', '$sce',
         {
             header: '',
             internal_title: 'Officers Description',
-            body: 'The officers of RPI Ambulance are elected at the first general body meeting of November. The ' +
-            'officers perform day to day and long term operational and administrative tasks. In 2009 the RPI Ambulance ' +
+            body: 'The officers of RPI Ambulance are elected each year in April. The ' +
+            'officers perform day to day and long term operational and administrative tasks. In 2009, the RPI Ambulance ' +
             'Constitution was changed to reduce the officer board from 8 officers to 5 consolidating "non-steering" ' +
             'positions into more important roles. This will allow for a smaller, more efficient officer board and the ' +
             'opportunity for more members to become involved with non-elected coordinator positions.'
@@ -13,7 +13,7 @@ angular.module('OfficersCtrl', []).controller('OfficersCtrl', ['$scope', '$sce',
 
 
         {
-            header: 'Officer Board 2018',
+            header: 'Officer Board 2019',
             internal_title: 'O-Board Photo',
             type: 'img',
             img: 'img/O-Board.jpg',
