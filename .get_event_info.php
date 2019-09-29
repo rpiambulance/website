0db831b3b8a94926942516f3173525405eada00e
Dan Bruce <ddbruce@users.noreply.github.com>
2019-09-29 15:24:39 -0400

    Fix event epoch issues

--- .get_event_info.php
+++ .get_event_info.php
@@ -35,7 +35,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   $results = array();
 
-  $sql = "SELECT *, UNIX_TIMESTAMP(start) AS start_epoch, UNIX_TIMESTAMP(end) AS end_epoch FROM events WHERE id = :eventId";
+  $sql = "SELECT *, UNIX_TIMESTAMP(CONCAT(CURRENT_DATE(), ' ', CONVERT(start, CHARACTER))) AS start_epoch, UNIX_TIMESTAMP(CONCAT(CURRENT_DATE(), ' ', CONVERT(end, CHARACTER))) AS end_epoch FROM events WHERE id = :eventId";
   $statement=$connection->prepare($sql);
   $statement->bindValue(':eventId', $eventId);
   $statement->execute();
