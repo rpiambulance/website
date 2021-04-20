02e8186291a4ed3d12093c4e6199bd6b187ea10a
Dan Bruce <ddbruce@users.noreply.github.com>
2021-04-19 21:34:57 -0400

    Update MiscFormsCtrl.js

--- js/controllers/MiscFormsCtrl.js
+++ js/controllers/MiscFormsCtrl.js
@@ -6,13 +6,13 @@ angular.module('MiscFormsCtrl', []).controller('MiscFormsCtrl', ['$scope', funct
         {
             header: 'Checklists',
             body: '<div class="embed-responsive embed-responsive-16by9"> ' +
-            '<iframe src="https://drive.google.com/embeddedfolderview?id=0BzoMrS_ZZPigVEJiNURXX2dPb0U#list" width="800" height="400" frameborder="0"></iframe>'
+            '<iframe src="https://drive.google.com/embeddedfolderview?id=1dBbb1WCLtVXzFoyBAZKhs_8dBqg6z62B#list" width="800" height="400" frameborder="0"></iframe>'
         },
 
         {
             header: 'Training Forms',
             body: '<div class="embed-responsive embed-responsive-16by9"> ' +
-            '<iframe src="https://drive.google.com/embeddedfolderview?id=0BzoMrS_ZZPigdk9ncFBaX2RwMzQ#list" width="800" height="400" frameborder="0"></iframe>'
+            '<iframe src="https://drive.google.com/embeddedfolderview?id=1OgqWm-RFUFoYASfRcgTRF3lwWbeWTpiT#list" width="800" height="400" frameborder="0"></iframe>'
 
         }
 
