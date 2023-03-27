c199232aaba76438bea9f3dbcbb8ab9f9bb99a8c
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2023-03-27 17:29:56 -0400

    Update Duty Supervisor description
    Per C1 request to match SOGs and update caps.

--- js/controllers/SupervisorTrainingCtrl.js
+++ js/controllers/SupervisorTrainingCtrl.js
@@ -23,7 +23,7 @@ angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function ($scope)
             internal_title: 'Duty Supervisor',
             body: 'The Duty Supervisor is on call 24 hours a day to handle any emergencies, incidents, or other issues ' +
             'that must be handled promptly, or to assist crews or members as needed. Duty Supervisors are the most ' +
-            'highly credentialed members in the agency. The on-duty Supervisor carries a dedicated cell phone (providing' +
+            'highly credentialed members in the agency. The on-duty supervisor carries a dedicated cell phone (providing' +
             ' members, DPS, other entities a way to always reach a knowledgeable person via phone – the cell phone is ' +
             'always able to be answered, and the number will not change with changes in supervisors, unlike changes in ' +
             'officers will). The supervisor also carries an RPIA portable and a portable 800, in order to be able to ' +
@@ -37,6 +37,7 @@ angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function ($scope)
             body: 'To be a Duty Supervisor a member must satisfy the following requirements:' + '\n' +
             '* Be a Crew Chief Trainer\n' +
             '* Be a Driver Trainer\n' +
+            '* Be a First Response Crew Chief\n' +
             '* Be an EES\n' +
             '* Have a 70% or higher QI compliance rate\n' +
             '* Be evaluated and approved by the Promotional Board consisting of the Captain and other active Duty Supervisors'
