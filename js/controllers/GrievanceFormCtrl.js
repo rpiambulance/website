9f5dc9d03cd7a95aab98158bc8b5f5491c60a8e2
Alex Mattoni <43799350+alexmattoni@users.noreply.github.com>
2024-03-12 09:54:33 -0400

    Final changes

--- js/controllers/GrievanceFormCtrl.js
+++ js/controllers/GrievanceFormCtrl.js
@@ -1,9 +1,8 @@
-var ctrl_name = 'GrievanceFormCtrl';
-angular.module(ctrl_name, []).controller(ctrl_name, ['$scope', function($scope) {
+angular.module('GrievanceFormCtrl', []).controller('GrievanceFormCtrl', ['$scope', function($scope) {
     $scope.pageHeader = 'RPI Ambulance Grievance Report Form';
     $scope.sections = [
         {
-            header: '',
+            header: 'Grievance Committee',
             internal_title: 'Grievance Committee',
             body: 'The Grievance Committee at RPIA aims to address interpersonal conflicts that may occur within the club, diffuse interagency tensions, and ensure that the SOGs and constitution are followed by all members. We stand as unbiased members of the club and hope that you’ll feel comfortable sharing any issues or concerns with us. We will work to the best of our abilities so that RPI Ambulance can be a great experience for all members. In many cases, a problem can be easily resolved by talking with each other. However, if you do not feel comfortable addressing the person(s) directly, please place your response through our Google form below so that we can help mediate a session with the other individual(s). There are many options in terms of level of anonymity, but all meetings and interactions related to your case will be held in a closed environment. Do not hesitate to contact us at gc@rpiambulance.com for more information.\n'
         },
